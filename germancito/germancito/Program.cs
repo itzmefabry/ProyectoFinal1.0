74a7febe60a3e71f939031b67fb6ba94eeb42425
itzmefabry <rasf080924hgtnrba0@gmail.com>
2025-11-28 22:03:36 -0600

    Lista de alumnosss

---
+++ germancito/germancito/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC11
+ * Date: 28/11/2025
+ * Time: 06:25 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace germancito
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
---
+++ germancito/germancito/menuprincipal.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC11
+ * Date: 28/11/2025
+ * Time: 06:26 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace germancito
+{
+	/// <summary>
+	/// Description of menuprincipal.
+	/// </summary>
+	public partial class menuprincipal : Form
+	{
+		public menuprincipal()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
