37edf2958a29b2b48924d696734d0afda443206e
GreenmanCL <darwinclhe@gmail.com>
2025-11-28 19:40:09 -0600

    valioshet

---
+++ germancito/germancito/germancito/MainForm.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC11
+ * Date: 28/11/2025
+ * Time: 06:25 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace germancito
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		// nose
+		bool xd = true;
+	}
+}
---
+++ germancito/germancito/germancito/formularioalumno.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC11
+ * Date: 28/11/2025
+ * Time: 06:26 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace germancito
+{
+	/// <summary>
+	/// Description of formularioalumno.
+	/// </summary>
+	public partial class formularioalumno : Form
+	{
+		public formularioalumno()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
