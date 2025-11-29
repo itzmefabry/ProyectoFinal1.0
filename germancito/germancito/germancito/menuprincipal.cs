8fd95a4b9507e507fb0d1aaf424017d7490e4853
ItzFabry <rasf080924hgtnrba0@cbtis217.edu.mx>
2025-11-28 21:36:08 -0600

    combinar

--- germancito/germancito/germancito/menuprincipal.cs
+++ germancito/germancito/germancito/menuprincipal.cs
@@ -30,3 +30,26 @@ namespace germancito
 		}
 	}
 }
+
+void BtnalumnosClick(object sender, EventArgs e)
+		{
+			formularioalumno f = new formularioalumno();
+			f.Show();
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+		
+		void BtnnumerosClick(object sender, EventArgs e)
+		{
+			numaleatorios f = new numaleatorios();
+			f.Show();
+		}
+		
+		void BtnpromedioClick(object sender, EventArgs e)
+		{
+			promedio f = new promedio();
+			f.Show();
+		}
