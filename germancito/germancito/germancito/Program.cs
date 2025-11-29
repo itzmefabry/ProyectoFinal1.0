1e8899977766ab9a3a5425ed4910455cfa8efd2d
itzmefabry <rasf080924hgtnrba0@gmail.com>
2025-11-28 23:09:51 -0600

    Lista  de alumnos

--- germancito/germancito/germancito/Program.cs
+++ germancito/germancito/germancito/Program.cs
@@ -24,7 +24,7 @@ namespace germancito
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MainForm());
+			Application.Run(new menuprincipal());
 		}
 		
 	}
